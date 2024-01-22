ef5b3c71b35fb63145d51db602e31a94441f13ca
Kaung Myat San <kaungmyatsan.cse@gmail.com>
2024-01-22 21:15:31 +0000

    assemble element stiffness matrix k and force vector f into the global stiffness matrix K and the global force vector F on 1d_meshing.m

--- 1D Meshing/1d_meshing.m
+++ 1D Meshing/1d_meshing.m
@@ -44,3 +44,20 @@ function [N, Nx] = CompElementShapeLinear1D(start_x, end_x, x_vector)
         Nx(2, i) = 1/(end_x - start_x); % N'2(x)
     end
 end
+
+% Assemble element stiffness matrix k and force vector f into the global
+% matrix K and vector F
+% Input: K, F: (empty) global stiffness matrix and force vector
+% Input: eid: element index in the mesh
+% Input: elements: the element matrix loaded from "elements.dat"
+% Input: k,f: local stiffness matrix and force vector
+% Outpyt: K, F: Assembled global stiffness matrix and force vector
+function [K, F] = AssembleGlobalLinear1D(K, F, eid, elements, k, f)
+    gids = [elements(eid, 2) elements(eid, 3)]';    % global indices of the 1st and 2nd nodes of element eid
+    for m = 1:2                             % loop over rows
+        F(gids(m)) = F(gids(m))+f(m);       % f(m) added onto global F
+        for n=1:2                           % loop over columns
+            K(gids(m), gids(n)) = K(gids(m), gids(n)+k(m, n);   % k(m,n) added onto K
+        end
+    end
+end
