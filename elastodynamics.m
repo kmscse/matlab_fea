3bdd17439a3a4b7d05af081449d4dda83abf5669
Kaung Myat San <kaungmyatsan.cse@gmail.com>
2024-01-22 18:50:33 +0000

    computing global force vector on elastodynamics.m

--- elastodynamics.m
+++ elastodynamics.m
@@ -89,3 +89,14 @@ for e=1:n_elements
     end
     M = AssembleGlobalMatrix(M, me, node_id_map, 2); % assemble global M
 end
+
+% Compute global force vector
+function F=CompF(nodes, elements, thickness, bcsforce)
+F = zeros(size(nodes, 1)*(size(nodes, 2)-1),1);
+n_force_nodes = size(bcsforce, 1);
+% for-loop: apply point forces
+for i=1:n_force_nodes
+    row = 2*bcsforce(i, 1)-1;
+    F(row, 1) = F(row, 1)+ bcsforce(i, 2)/thickness;
+    F(row+1, 1) = F(row+1, 1) + bcsforce(i, 3)/thickness;
+end
