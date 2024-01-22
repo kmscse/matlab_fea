eb896a9dbd8c0da0ab7acd8e174f56cd6783a203
Kaung Myat San <kaungmyatsan.cse@gmail.com>
2024-01-22 22:20:42 +0000

    define a function to compute the value of the 1-dimensional isoparametric shape functions and their first derivatives on 1d_meshing.m

--- 1D Meshing/1d_meshing.m
+++ 1D Meshing/1d_meshing.m
@@ -61,3 +61,19 @@ function [K, F] = AssembleGlobalLinear1D(K, F, eid, elements, k, f)
         end
     end
 end
+
+% Function to compute the value of the 1-D linear isoparametric shape functions and their first derivatives. 
+% The input vector xi_vector contains a list of points [xi] in the master element (between -1 and 1) where the shape functions should be evaluated. 
+% The values of the shape functions and their x-derivative are stored in N and Nx, respectively, in the format shown as follows.
+% N = [N1(xi1) N1(xi2) N1(xi3) ... 
+%       N2(xi1) N2(xi2) N2(xi3) ... ]
+% Nx = [N'1(xi1) N'1(xi2) N'1(xi3) ... 
+%       N'2(xi1) N'2(xi2) N'2(xi3) ... ]
+function [N, Nx] = CompShapeLinear1D(N,Nx,xi_vector)
+    n = size(xi_vector, 1);
+    for i=1:n
+        N(1,i) = (1-xi_vector(i))/2.0;      % N1(xi)
+        N(2,i) = (1+xi_vector(i))/2.0;      % N2(xi)
+        Nx(1:2, i) = [-0.5 0.5]';           % Nx(xi)
+    end
+end
